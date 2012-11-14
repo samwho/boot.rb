498727f84ac46b0521738d087cfc2382aef12607
Liam Dunn <liam.dunn144@gmail.com>
2012-11-14 13:02:27 +1100

    Added *.d to .gitignore and removed the existing *.d files

--- src/misc.d
+++
@@ -1,5 +0,0 @@
-src/misc.d src/misc.o: src/misc.c include/misc.h include/stdint.h
-
-include/misc.h:
-
-include/stdint.h:
--- src/screen.d
+++
@@ -1,8 +0,0 @@
-src/screen.d src/screen.o: src/screen.c include/screen.h include/misc.h \
-  include/stdint.h
-
-include/screen.h:
-
-include/misc.h:
-
-include/stdint.h:
