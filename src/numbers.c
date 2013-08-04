c34a973dfd46f9340546624647522f1bb9214941
Nick Markwell <nick@duckinator.net>
2013-08-04 18:19:02 -0400

    Fix atoi().

--- src/numbers.c
+++ src/numbers.c
@@ -3,8 +3,8 @@
 int atoi(const char *str)
 {
 	int ret = 0,
-	    pos = 0,
-	    tmp = end(str);
+	    pos = 0;
+	char *tmp = end(str);
 
 	for (pos = 0; tmp-- != str; pos++)
 		ret += (pos * 10) + (*tmp - 48);
